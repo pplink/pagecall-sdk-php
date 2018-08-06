d77c8da368bcd5742faef8deb722aef2a6db797a
webhacking <a@hax0r.info>
2018-08-06 11:44:01 +0900

    Add replay api

--- PageCall.php
+++ PageCall.php
@@ -64,7 +64,50 @@ class PageCall implements \PageCall\Interfaces\PageCall
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
             'Authorization: Bearer '. $this->accessToken->tokenInformation['token']
         ]);
-        $response = json_decode(curl_exec($ch), true);
+        $response = (array)json_decode(curl_exec($ch), true);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ( (int)substr($httpCode, 0, 1) !== 2 ) {
+            throw new PageCallSDKException(curl_error($ch), 401);
+        }
+        curl_close($ch);
+
+        return $response;
+    }
+
+    /**
+     * @param array $data
+     * @return array
+     * @throws PageCallAuthenticationException
+     * @throws PageCallSDKException
+     */
+    public function connectReplay(array $data = []): array
+    {
+        try {
+            AccessToken::validate($this->accessToken->tokenInformation);
+        } catch ( PageCallAuthenticationTokenExpiredException $e ) {
+            $this->accessToken = new AccessToken($this->config['accessKey'], $this->config['secretKey']);
+        }
+
+        if ( !$data['userId'] ) {
+            throw new PageCallSDKException('"userId" is required.');
+        }
+
+        if ( !$data['partnerId'] ) {
+            throw new PageCallSDKException('"partnerId" is required.');
+        }
+
+        if ( !$data['roomId'] ) {
+            throw new PageCallSDKException('"partnerId" is required.');
+        }
+
+        $ch = curl_init(__PAGE_CALL_API_ENDPOINT__.'connection/replay');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer '. $this->accessToken->tokenInformation['token']
+        ]);
+        $response = (array)json_decode(curl_exec($ch), true);
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
         if ( (int)substr($httpCode, 0, 1) !== 2 ) {
